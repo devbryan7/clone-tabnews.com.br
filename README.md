7e2073470f1d2890820ba1632c40dae1642284d3
Bryan Rovani <rrovanibryan@gmail.com>
2026-08-20 23:55:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews.com.br
+
 Este será o repositório onde vamos realizar o clone do curso do Felipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Oi mamis ! Eu te amo. Me diz que tá aparecendo uma mensagem pra ti kkkk</h1>
+function Home() {
+  return (
+    <h1>
+      Oi mamis ! Eu te amo. Me diz que tá aparecendo uma mensagem pra ti kkkk
+    </h1>
+  );
 }
 
 export default Home;
